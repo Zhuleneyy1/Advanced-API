00116229437cb673727ca195cc57833fb0d8c3eb
W0lfixicek <lotusekcz.business@gmail.com>
2024-04-22 18:33:32 +0200

    Opět změněn způsob Auth metody

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,24 +2,26 @@ const config = require('../config.json');
 const {Log} = require('./logger');
 
 const checkKey = async (req, res, next) => {
-    var key = req.headers['x-api-key'];
+    var apiKey = req.headers['x-api-key'];
     var ipAddress = req.ip;
 
-    if (config.app.key !== key) {
+    if (config.app.apiKey === apiKey) {
+        next();
+    } else {
         res.status(403).send('Nebyl zadán platný API klíč!');
         Log(`[${ipAddress}] Request attempt failed - invalid API Key!`);
     }
-    next();
 }
 
 const checkIP = async (req, res, next) => {
     var ipAddress = req.ip;
 
-    if (!config.addresses.includes(ipAddress)) {
+    if (ipAddress && config.addresses.includes(ipAddress)) {
+        next();
+    } else {
         res.status(403).send('Přístup z této IP adresy není oprávněný!');
         Log(`[${ipAddress}] Request attempt failed - unauthorized IP address!`);
     }
-    next();
 }
 
 module.exports = {checkKey, checkIP}
